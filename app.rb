08515031236d6ee009adad1bd061c2965743f923
vicfriedman <victoria.m.friedman@gmail.com>
2014-07-17 10:12:56 -0400

    update app.rb

--- app.rb
+++ app.rb
@@ -3,3 +3,7 @@ Bundler.require #require everything in bundler in gemfile
 require './lib/emailer'
 require './lib/scraper'
 
+
+get '/' do
+  erb :index # This tells your program to use the html associated with the index.erb file in your browser.
+end
