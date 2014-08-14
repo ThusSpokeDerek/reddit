8ed9fb87c88ad4c5ac5da72ea5df1b760c63cc55
dacosta1 <dacosta@cristoreyny.org>
2014-08-14 14:38:40 -0400

    almost

--- app.rb
+++ app.rb
@@ -1,9 +1,10 @@
 require 'bundler' #require bundler
 Bundler.require #require everything in bundler in gemfile
-require './lib/emailer'
-require './lib/scraper'
+require 'pry'
+require_relative 'lib/red.rb'
 
 
 get '/' do
+  @nokogiri_doc = Reddit.new 
   erb :index # This tells your program to use the html associated with the index.erb file in your browser.
 end
